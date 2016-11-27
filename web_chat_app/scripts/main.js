29ecc305d600131e864d72cb1d0a7de55417afb9
Diego Kremer <diego.kremer@gmail.com>
2016-11-27 14:03:21 -0200

    Correcao bug de botao de login
    Correcao do bug do botao que desaparecia

--- web_chat_app/scripts/main.js
+++ web_chat_app/scripts/main.js
@@ -149,23 +149,24 @@ OneChat.prototype.saveImageMessage = function(event) {
   }
 };
 
-/* // Signs-in OneChat.
+// Login com Google.
 OneChat.prototype.signIn = function() {
   // TODO(DEVELOPER): Sign in Firebase with credential from the Google user.
-  // Sign in Firebase using popup auth and Google as the identity provider.
+  // Login no Firebase usando autenticação com popup e Google como provedor de identidade.
   var provider = new firebase.auth.GoogleAuthProvider();
   this.auth.signInWithPopup(provider);
 };
-*/
 
-//Login com Facebook
+
+/* //Login com Facebook
 OneChat.prototype.signIn = function() {
+// Login no Firebase usando autenticação com popup e Facebook como provedor de identidade.
   var provider = new firebase.auth.FacebookAuthProvider();
   this.auth.signInWithPopup(provider);
   var token = result.credential.accessToken;
 
 };
-
+*/
 
 // Signs-out of OneChat.
 OneChat.prototype.signOut = function() {
@@ -283,42 +284,6 @@ OneChat.prototype.toggleButton = function() {
     this.submitButton.setAttribute('disabled', 'true');
   }
 };
-
-// Código de Cloud Messaging do Chrome
-
-function registerCallback(registrationId) {
-  if (chrome.runtime.lastError) {
-    // When the registration fails, handle the error and retry the
-    // registration later.
-    return;
-  }
-
-  // Send the registration token to your application server.
-  sendRegistrationId(function(succeed) {
-    // Once the registration token is received by your server,
-    // set the flag such that register will not be invoked
-    // next time when the app starts up.
-    if (succeed)
-      chrome.storage.local.set({registered: true});
-  });
-}
-
-function sendRegistrationId(callback) {
-  // Send the registration token to your application server
-  // in a secure way.
-}
-
-chrome.runtime.onStartup.addListener(function() {
-  chrome.storage.local.get("registered", function(result) {
-    // If already registered, bail out.
-    if (result["registered"])
-      return;
-
-    // Up to 100 senders are allowed.
-    var senderIds = ["Your-Sender-ID"];
-    chrome.gcm.register(senderIds, registerCallback);
-  });
-});
 
 // Checks that the Firebase SDK has been correctly setup and configured.
 OneChat.prototype.checkSetup = function() {
